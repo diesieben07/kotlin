8c20108a3d9ec0b85603e58b35b8a3d20d40a3fc
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2018-06-08 16:25:17 +0300

    'val' in 'when' subject can't have modifiers (annotations are ok)

--- compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
@@ -22515,6 +22515,11 @@ public class DiagnosticsUsingJavacTestGenerated extends AbstractDiagnosticsUsing
                     runTest("compiler/testData/diagnostics/tests/when/withSubjectVariable/smartcastToEnum.kt");
                 }
 
+                @TestMetadata("softModifierName.kt")
+                public void testSoftModifierName() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/when/withSubjectVariable/softModifierName.kt");
+                }
+
                 @TestMetadata("subjectVariableInIsPattern.kt")
                 public void testSubjectVariableInIsPattern() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/when/withSubjectVariable/subjectVariableInIsPattern.kt");
